be9fe2a8334a9cdf507bff7c8fa6c6e5bbc5e1be
sayan123234 <sayan123234@gmail.com>
2024-06-14 03:28:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,34 +29,11 @@ def generateResponse(dataFrame,prompt):
         answer = pandas_agent.chat(prompt)
         return answer
 
-st.write("# Collibra AI Meta-data Analyst")
-st.write("##### Engage in insightful conversations with your meta-data in Collibra (Business Glossary for Now) empowering you to uncover valuable insights and make informed decisions effortlessly!")
+st.write("# Talk with Novelis's Business Glossary")
+st.write("##### Engage in insightful conversations with your Glossary, empowering you to uncover valuable insights and make informed decisions effortlessly!")
 with st.sidebar:
-        #st.title("Collibra AI Meta-data Analyst")
-        #st.write("Engage in insightful conversations with your meta-data in Collibra (Business Glossary for Now) empowering you to uncover valuable insights and make informed decisions effortlessly!")
-        # Added a divider
-        st.divider()
-        # Add content to the sidebar/drawer
-        #with st.expander("Data Visualization"):
         st.write("Made with Gemini pro and pandas ai.")
         st.write("<div>Developed by - <span style=\"color: cyan; font-size: 24px; font-weight: 600;\">Sayan Sutradhar</span></div>",unsafe_allow_html=True)
-
-
-# uploaded_file = st.file_uploader("Upload your dataset here (excel)",type="xlsx")
-# if uploaded_file is not None:
-#         # Read the CSV file
-#         df = pd.read_excel(uploaded_file)
-
-#         # Display the data
-#         with st.expander("Preview"):
-#             st.write(df.tail())
-
-#         # Plot the data
-#         user_input = st.text_input("Type your message here",placeholder="Ask me about your data")
-#         if st.button("Generate"):
-#                 with st.spinner("Generating response..."):
-#                         answer = generateResponse(dataFrame=df,prompt=user_input)
-#                         st.write(answer)
 
 df = fetch_data()
 if df is not None:
